9cc4deb8596187787edd6d4516dcfb854e4639ee
Fara Aileen T. Blanco <fara.blanco@gmail.com>
2020-10-12 07:44:29 +0800

    step 4: Webpack Plugins and code linting with Standardjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const APP_DIR = path.join(__dirname, "src");
+
+const plugins = [
+  new HtmlWebPackPlugin({
+    template: `${APP_DIR}/index.html`,
+  }),
+  new CleanWebpackPlugin(),
+];
+
 module.exports = {
+  plugins,
   resolve: {
     extensions: [".jsx", ".js"],
   },
@@ -25,7 +39,7 @@ module.exports = {
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: "file-loader",
-      }
+      },
     ],
   },
 };
