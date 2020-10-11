e7952fc3903bd742e5182d3d3e9c4c761d14155b
Fara Aileen T. Blanco <fara.blanco@gmail.com>
2020-10-12 05:39:19 +0800

    step2: transpiling react with babel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-const App = () => (
-  <div>Hello world!</div>
-)
+import './App.scss'
+
+const App = () => <span className='lnr lnr-clock' />
 
 render(
   <App />,
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(jsx|js)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react'
-              ]
-            }
-          }
-        }
-      ]
-    }
-  }
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|svg)$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: "file-loader",
+      }
+    ],
+  },
+};
